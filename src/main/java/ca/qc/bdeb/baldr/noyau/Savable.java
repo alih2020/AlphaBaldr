e6511eee9ee81af84a99bb092cebae71d8340ed2
Hamza <hamza.ali@bdeb.qc.ca>
2022-02-17 09:25:10 -0500

    commit

--- src/main/java/ca/qc/bdeb/baldr/noyau/Savable.java
+++ src/main/java/ca/qc/bdeb/baldr/noyau/Savable.java
@@ -12,7 +12,7 @@ import org.w3c.dom.Node;
 /**
  * Interface for saving and restoring Baldr's components
  *
- * @see SaveAndRestore
+ //* @see SaveAndRestore
  * @author zeta
  */
 public interface Savable {
